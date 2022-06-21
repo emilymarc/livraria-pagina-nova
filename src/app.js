360a3947bbb9c1c440df5ef7b32540e4b8122556
Emily Marcolino <emily.marcolino@gmail.com>
2022-06-21 19:57:35 -0300

    add database connection

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 const express = require("express")
 const routes = require("./routes")
+const db = require("./database")
 
 const app = express()
+
+app.use(express.json())
+
+db.hasConnection()
 
 app.use(routes)
 
