55866911240b5ecab4c2abd6d3738721684c7c36
Emily Marcolino <emily.marcolino@gmail.com>
2022-06-21 19:58:11 -0300

    add db .env credentials

--- src/database/index.js
+++ src/database/index.js
@@ -6,7 +6,7 @@ const Sequelize = require("sequelize");
 
 const DB_NAME = process.env.DB_NAME;
 const DB_USER = process.env.DB_USER;
-const DB_PASS = "";
+const DB_PASS = process.env.DB_PASS;
 const DB_CONFIG = {
   dialect: process.env.DB_DIALECT,
   host: process.env.DB_HOST,
@@ -21,7 +21,7 @@ try {
   console.error("Error ao tentar uma conexão com banco dados");
 }
 
-async function hasConection() {
+async function hasConnection() {
   try {
     await db.authenticate();
     console.log("Banco dados conectado!");
@@ -31,7 +31,7 @@ async function hasConection() {
 }
 
 Object.assign(db, {
-  hasConection,
+  hasConnection,
 });
 
 module.exports = db;
