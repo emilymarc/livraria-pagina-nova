4b65076bb8c46d52554ca0d00e882f2097d6a9ba
Emily Marcolino <emily.marcolino@gmail.com>
2022-06-21 19:57:53 -0300

    add book creation - controller

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,6 +1,12 @@
+const { Books } = require("../database/models")
+
 const BookController = {
-    async create(req,res){
-        res.send('oi')
+    async create(req, res){
+        const newBook = await Books.create({
+            ...req.body
+        })
+
+        return res.status(201).json(newBook)
     }
 }
 
