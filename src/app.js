8e326c0450c91024180f79fcc8430f0e2d7f9efc
Emily Marcolino <emily.marcolino@gmail.com>
2022-06-23 19:59:51 -0300

    add multer configs to upload files in server

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const routes = require("./routes")
 const db = require("./database")
+const upload = require("./config/upload")
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
+app.use(upload.any())
 
 db.hasConnection()
 
