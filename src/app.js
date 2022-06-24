ff9254889b814619ed0fac45bb057156208b460d
Emily Marcolino <emily.marcolino@gmail.com>
2022-06-23 21:05:27 -0300

    add link statico para rotas de imagens

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ const express = require("express")
 const routes = require("./routes")
 const db = require("./database")
 const upload = require("./config/upload")
+const { use } = require("./routes")
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 app.use(upload.any())
+app.use(express.static('src/public/images'))
 
 db.hasConnection()
 
